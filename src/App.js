37e5d063a1a416cd7c5287322fd2ce3ce0157168
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-05-21 23:34:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-/* 
-Start here and then work through the 
-PostsContainer components and the CommentSection Components. 
-Not all files need code added. 
-Look at each file to see where you need to pass props or add code 
-*/
-import React from "react";
-import "./App.css";
-// import the PostsPage and SearchBar and add them to the App
-
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+import PostPage from './components/PostsContainer/PostsPage';
+import './App.css';
 
 const App = () => {
-
-  return (
-    <div className="App">
-      {/* Add imported components here to render them */}
-    </div>
-  );
+	return (
+		<div className='App'>
+			<SearchBar />
+			<PostPage />
+		</div>
+	);
 };
 
 export default App;
